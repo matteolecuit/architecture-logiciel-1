d579ff12d210107647a550dc790d4947d89b5ac3
Matteo Lecuit <matteo@lecuit.fr>
2021-10-22 15:00:13 +0200

    create type

--- dao/ProductDAO.js
+++ dao/ProductDAO.js
@@ -6,8 +6,24 @@ module.exports = {
     return products;
   },
   create: (product) => {
-    product._id = PRODUCT_COUNT;
-    FAKE_DB.products.push(product);
+    try {
+      if (
+        typeof product.name !== "string" ||
+        product.name.length < 3 ||
+        product.name.length > 100
+      )
+        throw new Error("BAD_PARAMETERS");
+      if (typeof product.description !== "string")
+        throw new Error("BAD_PARAMETERS");
+      product.price = Number(product.price);
+      if (typeof product.price !== "number" || product.price < 0)
+        throw new Error("BAD_PARAMETERS");
+      product._id = PRODUCT_COUNT;
+      FAKE_DB.products.push(product);
+      PRODUCT_COUNT += 1;
+    } catch (e) {
+      console.log(e);
+    }
   },
   delete: ({ id }) => {
     let index = FAKE_DB.products.findIndex((e) => e._id === id);
